03e9c2aa37f58d5a93bc63ee258535840a180e26
Dima <dima.semenovych@gmail.com>
2026-09-04 18:07:53 +0100

    Complete MongoDB CRUD homework

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,21 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  console.error(err);
+
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      message: err.message,
+    });
+
+    return;
+  }
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+};
